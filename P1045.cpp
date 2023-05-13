dfb9884cf3a36728f3b6f1f04be88e19247ce677
zwq <941454643@qq.com>
2023-05-13 21:19:02 +0800

    2023/5/13/21/18

--- P1045.cpp
+++ P1045.cpp
@@ -1,40 +1,60 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-int a[10000000]={0};
+long long a[501]={0};
+long long cp(int n)
+{
+    long long result=1;
+    for (int i = 1; i <= n; i++)
+    {
+        result*=2;
+    }
+    return result;
+}
 int main()
 {
-    int p,c=1;
+    int p;
     cin>>p;
-    a[c]=1;
-    for (int i = 0; i < p; i++)
+    int num1=p/32;
+    int num2=p-num1*32;
+    long long x=cp(32);
+    a[500]=1;
+    /**
+     * @brief 
+     * 为了加快速度，我们在前num1轮对每个数乘以2^32，由于p/32并不一定刚好整除，因此我们在剩下的num1轮当中对每个数每次乘2
+     */
+    for (int i = 1; i <= num1; i++)
     {
-        int jw=0,j;
-        for (j = 1; j <= min(c,500); j++)
+        for (int j = 500; j >= 1; j--)
         {
-            int sum=a[j]*2+jw;
-            a[j]=sum%10;
-            jw=sum/10;
-        }        
-        if (c<=500)
-        {
-            while (jw>0)
-            {
-                a[j]=jw%10;
-                jw/=10;
-                j++;
-            }
+            a[j]*=x;
         }
-        c=j-1;
-    }  
-    a[1]-=1;
-    cout<<(int)(p*log10(2)+1)<<endl;
-    for (int i = 500; i >= 1; i--)
-    {
-        printf("%d",a[i]);
-        if ((501-i)%50==0)
+        for (int j = 500; j >= 1; j--)
         {
-            printf("\n");
+            a[j-1]+=a[j]/10;
+            a[j]%=10;
+        }
+    }
+    for (int i = 1; i <= num2; i++)
+    {
+        for (int j = 500; j >= 1; j--)
+        {
+            a[j]*=2;
+        }
+        for (int j = 500; j >= 1; j--)
+        {
+            a[j-1]+=a[j]/10;
+            a[j]%=10;
+        }
+    }
+    a[500]--;
+    cout<<(int)(p*log10(2)+1)<<endl;
+    for (int i = 1; i <= 500; i++)
+    {
+        cout<<a[i];
+        if (i%50==0)
+        {
+            cout<<endl;
         }
     }
 }
